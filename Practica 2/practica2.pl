e08c89d26bf5cdbc3a6d5fd8a97750696cc45d0e
Oscar V.R <ferosre@gmail.com>
2023-04-24 20:42:16 +0000

    Update practica2.pl

--- Practica 2/practica2.pl
+++ Practica 2/practica2.pl
@@ -1,36 +1,34 @@
+% Oscar Vilela Rodriguez (oscar.vilela.rodriguez@udc.es)
+% Guillermo García Engelmo (g.garcia2@udc.es)
+
+% Operadores lógicos en prolog
 :- op(1060, yfx, <->).  % doble implicación
 :- op(1050, yfx, <-).   % implicación hacia la izquierda
 :- op(800, yfx, xor).   % disyunción exclusiva
 :- op(600, yfx, v).     % disyunción
 :- op(400, yfx, &).     % conjunción
 :- op(200, fy, ~ ).     % negación
-:- op(800, yfx, nand).
 
-% Definición de los átomos
+
+
+% Apartado 1: operadores derivados (predicado unfold)
 define((F xor G), ~ (F <-> G)).
 define((F <-> G), (F -> G) & (F <- G)).
 define((F <- G), (G -> F)).
 define((F -> G), ~ F v G).
-define((F nand G), ~ (F & G)).
 
-
-%?- unfold( p <-> q & ¬ r, G ).
-%G =  (¬p v q& ¬r)&(¬ (q& ¬r)v p).
-
-%?- unfold( (a -> b) xor c, G ).
-%G = ¬ ((¬ (¬a v b)v c)&(¬c v (¬a v b))).
-
-
-% Predicado unfold para transformar fórmulas en su equivalente con los operadores permitidos
 
 unfold(F,G) :- atom(F), G=F. % Si F es un átomo, entonces G será igual a F.
+
 
 unfold(~F, ~G) :- % Si F es una negación, se aplicará la equivalencia de la negación.
     unfold(F, G), !.
 
+
 unfold((F & G), (H & J)) :- % Si F y G están unidos por la conjunción, se aplicará la equivalencia de la conjunción.
     unfold(F, H),
     unfold(G, J).
+
 
 unfold((F v G), (H v J)) :- % Si F y G están unidos por la disyunción, se aplicará la equivalencia de la disyunción.
     unfold(F, H),
@@ -41,66 +39,43 @@ unfold(F, G) :- % Si F no es un átomo ni una fórmula con los operadores permit
     unfold(NewF, G), !. % Se llama de manera recursiva al predicado unfold con la nueva fórmula NewF.
 
 
-%unfold(F v G, H v K) :- unfold(F, H), unfold(G, K).
-% ~a v ~ (~b v c).
+% Apartado 2: Tablas Semánticas
+
+
 tab(F, R) :-
-    unfold(F, G),
-    tab([G], [], R). % tab([~a v ~ (~b v c).], [], R)
+    unfold(F, G),   
+    tab([G], [], R).    
 
-tab([F], Ls1, [F|Ls1]) :- %tab([b], [], [b])
+tab([F], Ls1, [F|Ls1]) :-  % Si la fórmula F es atómica, entonces se agrega a la lista Ls1 que se devuelve como tercer argumento
     atomic(F).
 
-tab([~F], Ls1, [~F|Ls1]) :- %tab([~c], [b], [~c,b])
+
+tab([~F], Ls1, [~F|Ls1]) :- % Si F es una negación de una fórmula, se agrega a Ls1 como tercera entrada
     atomic(F).
 
-tab([~(~F)], Ls1, Ls2) :-
-    tab([F], Ls1, Ls2). 
 
-tab([F & G], Ls1, Ls3) :-
-    tab([F], Ls1, Ls2), %tab([b], [], Ls2) Ls2 =[b]
-    tab([G], Ls2, Ls3), %tab([~c], [b], Ls3) Ls3 =[~c, b]
-    tab(Ls3). % de ultimo comprueba toda la rama, para ver si hay algún átomo y su negación
+tab([~(~F)], Ls1, Ls2) :-  % Si F es la doble negación de una fórmula, se llama al predicado 'tab/3' con F como entrada y Ls2 como salida
+    tab([F], Ls1, Ls2).
 
-tab([~(F & G)], Ls1, Ls2) :-
+
+tab([F & G], Ls1, Ls3) :-    % Si F y G son conjunciones de dos fórmulas, se llama a 'tab/3' con F y G como entrada, y se agrega el resultado a Ls3
+     tab([F], Ls1, Ls2),     % Llama a 'tab/3' con F como entrada y Ls2 como salida
+     tab([G], Ls2, Ls3),     % Llama a 'tab/3' con G como entrada y Ls3 como salida
+     tab(Ls3).               % Llama a 'tab/1' con Ls3 como entrada
+
+
+tab([~(F & G)], Ls1, Ls2) :- % Si F y G son conjunciones y se niegan, se llama al predicado 'tab/3' con las fórmulas ~F y ~G unidas por una disyunción como entrada, y se devuelve Ls2
     tab([~F v ~G], Ls1, Ls2).
 
-tab([F v G], Ls1, Ls2) :-
-    (tab([F], Ls1, Ls2) ; tab([G], Ls1, Ls2)). %F = tab([~a], [], Ls2) , tab([~ (~b v c).], [], Ls2)
-    
-tab([~(F v G)], Ls1, Ls2) :-
-    tab([~F & ~G], Ls1, Ls2). %tab([(b & ~c).], [], Ls2)
-
-tab(Ls3) :-
-    \+ (select(A, Ls3, Rest), memberchk(~A, Rest)).
 
 
-% Fs lista de fórmulas que aún tenemos que desdoblar en la tabla semántica, 
-% Lits1 conjunto de literales que llevamos calculados en la rama actual hasta el momento, si llegamos al final (no quedan fórmulas por desdoblar) la lista
-% Lits2 devuelve la rama que hemos encontrado abierta. Si todas las ramas están cerradas (fórmula inconsistente) tanto tab/3 como, por tanto, tab/2 deberán ser false.
-
-% [1,2,3,4] = [1|[2|[3|[4|[]]]]
-
-tab(((~p) & q) -> (p & (q v (~r))), R).
+tab([F v G], Ls1, Ls2) :-   % Si F y G son disyunciones, llama a 'tab/3' con F y G como entrada, y se agrega el resultado a Ls2
+    (tab([F], Ls1, Ls2) ; tab([G], Ls1, Ls2)). % Llama a 'tab/3' con F o G como entrada y Ls2 como salida, dependiendo del resultado de la disyunción lógica
 
 
+tab([~(F v G)], Ls1, Ls2) :- % Si F y G son disyunciones y se niegan, llama a 'tab/3' con ~F y ~G unidos por una conjunción como entrada y se devuelve Ls2
+    tab([~F & ~G], Ls1, Ls2).
 
 
-
-
-
-
-
-
-
-
-%unfold(F, G) :- F =.. [Op| [Arg1, Arg2]], unfold(Arg1, G1), unfold(Arg2, G2), G =.. [Op, G1, G2].
-
-%unfold(F, X1 v X2) :- F=.. [Op|[F1|F2]] , unfold(F1,X1), unfold(F2,X2).
-
-% 1+1=.. L.
-% L=[+,1,1]
-
-%familia(leonor, felipe, juancarlos)
-%F=[familia, leonor, felipe, juancarlos]
-
-%F= a v b, F=.. [0p, F1, F2]
+tab(Ls3) :-  % Si Ls3 es una lista de fórmulas, verifica si contiene una fórmula y su negación en Ls3
+    \+ (select(A, Ls3, Rest), memberchk(~A, Rest)). % Si no hay tal par, devuelve verdadero; de lo contrario, devuelve falso
