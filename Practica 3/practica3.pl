aa61f4180dbb5b5eeead3fe1177936502f3a4d1c
Oscar V.R <ferosre@gmail.com>
2023-04-28 02:55:48 +0000

    Update practica3.pl

--- Practica 3/practica3.pl
+++ Practica 3/practica3.pl
@@ -7,8 +7,11 @@
 :- op(200, fx, exists).  % existencial
 :- op(300, xfy, ::).     % separador para cuantificadores
 
-subs(X/T,X,T). 
-subs(X/T,Y,Y) :- atom(Y), X=Y. 
-subs(X/T,F,G) :- F =.. [H|T1], subs_list(X/T,T1,T2), G =.. [H|T2]. 
-subs(X/T,F,F) :- atomic(F), !. subs_list(_,[],[]). 
-subs_list(X/T,[H|T1],[H2|T2]) :- subs(X/T,H,H2), subs_list(X/T,T1,T2).
+
+%?- subs(x/f(3), g(x), G). %f(3) no es atomico, g(x) no es atomico
+%Casos base
+subs(X/T, X, T).
+subs(X/T, F, G) :-
+    F =.. [Op|Args],
+    maplist(subs(X/T), Args, Args1), 
+    G =.. [Op|Args1], !.
