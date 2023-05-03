20ee2a444ed3113624ff2df9cb42cf37cdfcd4c6
Oscar V.R <ferosre@gmail.com>
2023-05-03 23:31:26 +0000

    Update practica3.pl

--- Practica 3/practica3.pl
+++ Practica 3/practica3.pl
@@ -11,18 +11,39 @@
 %?- subs(x/f(3), g(x), G). %f(3) no es atomico, g(x) no es atomico
 
 %Casos base
-subs(X/T, X, T):- !.                  %  z/f(3), y, G
-subs(X/T, H, H):- atom(H), !.           %  x/f(3), y, G
+subs(X/T, X, T):- !.                      %  z/f(3), y, G
+subs(X/T, H, H):- atom(H), !.             %  x/f(3), y, G
 subs(X/T, exists X::F, exists X::F):- !.  % casos para cuantificadores
 subs(X/T, forall X::F, forall X::F):- !.
 
 % Casos recursivos
 subs(X/T, F, G) :-                   
     F =.. [Op|Args],
-    maplist(subs(X/T), Args, NewArgs), %Aplica el subs a cada elemento de la lista
+    substitute(subs(X/T), Args, NewArgs), %Aplica el subs a cada elemento de la lista
     G =.. [Op|NewArgs].
 
 
 % L =[subs, a/f(3), a+4], X=..L.
 % L =[subs, a/f(3), a+4]
 % X = subs(a/f(3), a+4).
+
+%Otra implementación:
+
+% Caso base
+subs(X/T, X, T):- !.                   % z/f(3), y, G
+subs(X/T, H, H):- atom(H), !.          % x/f(3), y, G
+subs(X/T, exists X::F, exists X::F):- !. % casos para cuantificadores
+subs(X/T, forall X::F, forall X::F):- !.
+
+% Caso recursivo
+subs(X/T, F, G) :-                   
+    F =.. [Op|Args],
+    subs_list(X/T, Args, NewArgs),      % Aplica subs a la lista de argumentos
+    G =.. [Op|NewArgs].
+
+% subs_list(+X/T, +Args, -NewArgs)
+% Aplica subs a cada elemento de Args y construye una nueva lista con los resultados
+subs_list(_, [], []).
+subs_list(X/T, [A|As], [B|Bs]) :-
+    subs(X/T, A, B),
+    subs_list(X/T, As, Bs).
